defd83bb1ccda98bc50cf5c735fd8f2c2d66b968
mkaraki <masakazu.araki@mkarakiapps.com>
2022-11-30 18:56:12 +0900

    Add stats page for try mode

--- try.php
+++ try.php
@@ -7,6 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>沙花叉Captchaを試す</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        img {
+            max-width: 100%;
+        }
+    </style>
 </head>
 
 <body class="p-1">
@@ -16,7 +21,12 @@
             <?php if (validate()) : ?>
                 <div class=" alert alert-success" role="alert">
                     あなたは人間でしょう。
+                    <a href="mystats.html" class="alert-link">正答率を確認</a>
                 </div>
+                <script>
+                    if ((localStorage.getItem('lastSeed') ?? '0') != '<?= $seed ?>')
+                        localStorage.setItem('correct', parseInt(localStorage.getItem('correct') ?? '0') + 1);
+                </script>
             <?php else : ?>
                 <div class="alert alert-danger" role="alert">
                     あなたはロボットかも知れません。
@@ -25,8 +35,17 @@
                         入力: <code><?= htmlspecialchars($_POST['input']) ?></code>
                         正解: <code><?= $string ?></code>
                     </small>
+
+                    <a href="mystats.html" class="alert-link">正答率を確認</a>
                 </div>
+                <script>
+                    if ((localStorage.getItem('lastSeed') ?? '0') != '<?= $seed ?>')
+                        localStorage.setItem('incorrect', parseInt(localStorage.getItem('incorrect') ?? '0') + 1);
+                </script>
             <?php endif; ?>
+            <script>
+                localStorage.setItem('lastSeed', '<?= $seed ?>')
+            </script>
         <?php endif; ?>
     </div>
     <?php
@@ -44,7 +63,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="captchainput" class="form-label">文字を入力</label>
-                        <input type="text" class="form-control" id="captchainput" name="input">
+                        <input type="text" class="form-control" id="captchainput" name="input" maxlength=5 minlength="5" required>
                     </div>
                     <input type="hidden" name="seed" value="<?= $seed ?>">
                     <button type="submit" class="btn btn-primary">次へ</button>
