bc9cb901f9c7a4637ba7c914773d19cf6fede6ac
mkaraki <masakazu.araki@mkarakiapps.com>
2022-11-30 19:50:04 +0900

    Remove production level captcha system

--- api/validate.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ . '/../req/validate.php');
-header("Content-type: text/json");
-
-print json_encode(array('result' => validate() ? 'ok' : 'ng', 'actual' => $string, 'input' => $_POST['input']), JSON_UNESCAPED_UNICODE);
